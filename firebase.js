d0a30dd32eb2ae1d1edd1ca526f915edafc43fb8
shabir-zahir <shabirzahir1202@gmail.com>
2024-08-08 18:39:28 -0400

    commit

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 import {getFirestore, getFireStore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +7,14 @@ import {getFirestore, getFireStore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAuc-DYPll75rvGVdbXA_aEnBbOC23nPjA",
-  authDomain: "pantry-tracker-9d5ad.firebaseapp.com",
-  projectId: "pantry-tracker-9d5ad",
-  storageBucket: "pantry-tracker-9d5ad.appspot.com",
-  messagingSenderId: "486896697870",
-  appId: "1:486896697870:web:db659c49d9b8f1ad3cb25f",
-  measurementId: "G-4VSXVMZZWY"
+   
+  apiKey: //INFORMATION,
+  authDomain: //INFORMATION,
+  projectId: //INFORMATION,
+  storageBucket: //INFORMATION,
+  messagingSenderId://INFORMATION,
+  appId: //INFORMATION,
+  measurementId: //INFORMATION
 };
 
 // Initialize Firebase
